b8eb07525d4311b39888fa1288b2af6ed9af8733
Anson Eberle <anson.eberle@gmail.com>
2026-09-23 15:40:07 -0400

    Deleted test Pedro Constants and test for the Students to do

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -13,23 +13,6 @@ import org.firstinspires.ftc.teamcode.pedro.procedures.Tests;
 
 public class Tuning {
     // Tuners go here
-    @Tuner
-    public static Procedure mecanumTuner() {
-        return new MecanumTuner();
-    }
-    @Tuner
-    public static Procedure tests() {
-        return new Tests(hardwareMap -> new Mecanum(hardwareMap, Constants.drivetrainConfig), (hardwareMap -> new PinpointLocalizer(hardwareMap, Constants.localizerConfig)), () -> new Foresight(Constants.foresightConfig));
-    }
-    @Tuner
-    public static Procedure pinpointTuner() {
-        return new PinpointTuner();
-    }
-    @Tuner
-    public static Procedure foresightTuner() {
-        return new ForesightTuner((hardwareMap) -> new PinpointLocalizer(hardwareMap, Constants.localizerConfig), (hardwareMap) -> new Mecanum(hardwareMap, Constants.drivetrainConfig));
-    }
-
 
 }
 
